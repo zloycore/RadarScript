a9bd15bd33e1b1da8bb7cc2a963ce3468e96a1e8
zloycore <zloycut8923@gmail.com>
2025-08-20 02:15:21 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,8 @@
+import os
 
+class Config:
+    API_ID = os.getenv('API_ID', '25293525')
+    API_HASH = os.getenv('API_HASH', '2fc1293e908b08187d519f79bb046018')
+    PHONE_NUMBER = os.getenv('PHONE_NUMBER', '+13853495776')
+
+config = Config()
